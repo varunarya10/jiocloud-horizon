e65bd4febe6c8f66ae213ceaf82570f9229b02be
hkumarmk <hkumarmk@gmail.com>
2014-02-18 05:25:10 +0000

    Ensure changed from installed to latest
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/module_source/puppet-horizon@161 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/init.pp
+++ manifests/init.pp
@@ -121,7 +121,7 @@
 class horizon(
   $secret_key,
   $fqdn                    = $::fqdn,
-  $package_ensure          = 'present',
+  $package_ensure          = 'latest',
   $cache_server_ip         = '127.0.0.1',
   $cache_server_port       = '11211',
   $swift                   = false,
