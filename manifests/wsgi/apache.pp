5222ed3536d217b4b43264f4f34b008f890b74cf
hkumarmk <hkumarmk@gmail.com>
2014-03-18 17:40:09 +0000

    added apache module access_compat
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/module_source/puppet-horizon@275 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -30,6 +30,7 @@ class horizon::wsgi::apache (
   include ::horizon::params
   include ::apache
   include ::apache::mod::wsgi
+  include ::apache::mod::access_compat
 
 ### Required for apache 2.4
 #  include ::apache::mod::authn_core
